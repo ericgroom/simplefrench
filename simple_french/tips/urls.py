1032035e33f610667f496328312a110ce8f376dc
Eric Groom <bcgroom@gmail.com>
2018-03-10 10:59:08 -0800

    add delete view

--- simple_french/tips/urls.py
+++ simple_french/tips/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.TipListView.as_view(), name='list'),
     path('<pk>', views.TipDetailView.as_view(), name='detail'),
     path('<pk>/edit', views.TipUpdateView.as_view(), name='update'),
+    path('<pk>/delete', views.TipDeleteView.as_view(), name='delete'),
 ]
--- simple_french/tips/views.py
+++ simple_french/tips/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Tip
+
+import pdb
 
 # Create your views here.
 class TipListView(ListView):
@@ -14,3 +18,8 @@ class TipDetailView(DetailView):
 class TipUpdateView(LoginRequiredMixin, UpdateView):
     fields = ['title', 'content',]
     model = Tip
+
+class TipDeleteView(DeleteView):
+    model = Tip
+    success_url = reverse_lazy('tips:list')
+    
